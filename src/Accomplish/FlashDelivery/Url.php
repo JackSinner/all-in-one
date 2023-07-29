8a0d39afb11ade8edf2b9e554c19b96ddd1824bb
袁子强 <643145444@qq.com>
2023-07-29 17:57:10 +0800

    修改闪送请求地址

--- src/Accomplish/FlashDelivery/Url.php
+++ src/Accomplish/FlashDelivery/Url.php
@@ -5,20 +5,20 @@ namespace Library\Europe\Accomplish\FlashDelivery;
 class Url
 {
     //订单计费
-    const ORDER_CALCULATE_URI = 'http://open.s.bingex.com/openapi/merchants/v5/orderCalculate';
+    const ORDER_CALCULATE_URI = 'https://open.ishansong.com/openapi/merchants/v5/orderCalculate';
 
     //提交订单
-    const ORDER_PLACE_URI = 'http://open.s.bingex.com/openapi/merchants/v5/orderPlace';
+    const ORDER_PLACE_URI = 'https://open.ishansong.com/openapi/merchants/v5/orderPlace';
 
     //查询账号额度
-    const GET_USER_ACCOUNT_URI = 'http://open.s.bingex.com/openapi/merchants/v5/getUserAccount';
+    const GET_USER_ACCOUNT_URI = 'https://open.ishansong.com/openapi/merchants/v5/getUserAccount';
 
     //修改收件人手机号
-    const UPDATE_TO_MOBILE_URI = 'http://open.s.bingex.com/openapi/merchants/v5/updateToMobile';
+    const UPDATE_TO_MOBILE_URI = 'https://open.ishansong.com/openapi/merchants/v5/updateToMobile';
 
     //订单取消
-    const ABORT_ORDER_URI = 'http://open.s.bingex.com/openapi/merchants/v5/abortOrder';
+    const ABORT_ORDER_URI = 'https://open.ishansong.com/openapi/merchants/v5/abortOrder';
 
     //确认送回
-    const CONFIRM_GOODS_RETURN_URI = 'http://open.s.bingex.com/openapi/merchants/v5/confirmGoodsReturn';
+    const CONFIRM_GOODS_RETURN_URI = 'https://open.ishansong.com/openapi/merchants/v5/confirmGoodsReturn';
 }
